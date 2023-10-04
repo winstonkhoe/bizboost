437696b32ca60b1514cc15d8455255145bb511ee
Winston <winston@gdn-commerce.com>
2023-10-05 00:55:28 +0700

    feat: add svg import support, fix migrations

---
+++ src/containers/SafeAreaContainer.tsx
@@ -0,0 +1,22 @@
+import {ReactNode} from 'react';
+import {View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+type Props = {
+  children: ReactNode;
+};
+const SafeAreaContainer = ({children}: Props) => {
+  const insets = useSafeAreaInsets();
+  return (
+    <View
+      style={{
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+      }}>
+      {children}
+    </View>
+  );
+};
+
+export default SafeAreaContainer;
---
+++ src/screens/WelcomeScreen.tsx
@@ -0,0 +1,22 @@
+import {Button, Text, View} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../navigation/GuestNavigation';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Welcome'>;
+const WelcomeScreen = ({navigation}: Props) => {
+  return (
+    <View className="h-full">
+      <View className="container h-full justify-center items-center text-center">
+        <Text>This is WelcomeScreen</Text>
+        <Button
+          title="Home"
+          onPress={() => {
+            navigation.navigate('Authenticated');
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
