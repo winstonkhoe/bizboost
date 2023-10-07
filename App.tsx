0e633e2a93e3f68b34723886ce788e5f9ed43936
je-von <jevon.levin@gmail.com>
2023-10-07 21:41:28 +0700

    fix: listen to authentication state in App.tsx

--- App.tsx
+++ App.tsx
@@ -5,18 +5,35 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import GuestNavigation from './src/navigation/GuestNavigation';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import AuthenticatedNavigation from './src/navigation/AuthenticatedNavigation';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+const App = () => {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
 
-const App = (): JSX.Element => {
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(userState => {
+      setUser(userState);
+      if (initializing) {
+        setInitializing(false);
+      }
+    });
+    return subscriber;
+  });
+  if (initializing) {
+    return null;
+  }
+
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <SafeAreaProvider>
         <NavigationContainer>
-          <GuestNavigation />
+          {!user ? <GuestNavigation /> : <AuthenticatedNavigation />}
         </NavigationContainer>
       </SafeAreaProvider>
     </GestureHandlerRootView>
