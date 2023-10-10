ee36e2d47a17587613c8726f29c7baea21b12516
Winston <winstonkhoe@yahoo.com>
2023-10-10 19:40:39 +0700

    feat: add switch account modal

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import GuestNavigation from './src/navigation/GuestNavigation';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
@@ -13,6 +13,15 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AuthenticatedNavigation from './src/navigation/AuthenticatedNavigation';
 import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet';
+import {View} from 'react-native';
+import {AccountListCard} from './src/components/molecules/AccountListCard';
+import {flex} from './src/styles/Flex';
+import {gap} from './src/styles/Gap';
 
 GoogleSignin.configure({
   webClientId:
@@ -21,8 +30,11 @@ GoogleSignin.configure({
 const App = () => {
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
   useEffect(() => {
+    bottomSheetModalRef.current?.present();
+    // bottomSheetModalRef.current?.expand();
     const subscriber = auth().onAuthStateChanged(userState => {
       setUser(userState);
       if (initializing) {
@@ -41,6 +53,19 @@ const App = () => {
         <NavigationContainer>
           {!user ? <GuestNavigation /> : <AuthenticatedNavigation />}
         </NavigationContainer>
+        <BottomSheetModalProvider>
+          <BottomSheetModal
+            ref={bottomSheetModalRef}
+            enableDynamicSizing
+            enablePanDownToClose>
+            <BottomSheetView>
+              <View className="pt-2 pb-6" style={[flex.flexCol, gap.default]}>
+                <AccountListCard name="DavidCoderz" active={true} role="CC" />
+                <AccountListCard name="kodetime" active={false} role="BP" />
+              </View>
+            </BottomSheetView>
+          </BottomSheetModal>
+        </BottomSheetModalProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
