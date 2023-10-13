8caf4494366628ae57015464156311ab03c0e461
Winston <winstonkhoe@yahoo.com>
2023-10-14 03:09:19 +0700

    feat: add profile page (BIZ-16)

--- src/hooks/user.ts
+++ src/hooks/user.ts
@@ -1,11 +1,35 @@
 import {useEffect} from 'react';
 import {useAppDispatch, useAppSelector} from '../redux/hooks';
 import {setUser} from '../redux/slices/userSlice';
-import {User} from '../model/User';
+import {
+  BusinessPeople,
+  ContentCreator,
+  User,
+  UserRole,
+  UserRoles,
+} from '../model/User';
 
 export const useUser = () => {
   const {user, activeRole, uid} = useAppSelector(state => state.user);
   const dispatch = useAppDispatch();
+  const getData = (
+    user: User | null,
+    activeRole: UserRoles,
+  ): ContentCreator | BusinessPeople | undefined => {
+    if (!user) {
+      return undefined;
+    }
+    if (activeRole === UserRole.ContentCreator) {
+      return user?.contentCreator;
+    } else if (activeRole === UserRole.BusinessPeople) {
+      return user?.businessPeople;
+    }
+    return undefined;
+  };
+  const activeData: ContentCreator | BusinessPeople | undefined = getData(
+    user,
+    activeRole,
+  );
 
   useEffect(() => {
     const updateUserState = (u: User | null, unsubscribe: () => void) => {
@@ -18,5 +42,5 @@ export const useUser = () => {
       User.getUserDataReactive(uid, updateUserState);
     }
   }, [user, uid, dispatch]);
-  return {uid, user, activeRole};
+  return {uid, user, activeRole, activeData};
 };
