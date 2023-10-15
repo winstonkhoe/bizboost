8c304698a43b4e6f27fb1a80b8833f79d9dd810d
Winston <winstonkhoe@yahoo.com>
2023-10-16 03:29:35 +0700

    feat: retrieve user instagram data

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -3,11 +3,119 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootGuestStackParamList} from '../navigation/GuestNavigation';
 import {Button} from 'react-native-elements';
 import Logo from '../assets/images/bizboost.svg';
+import {
+  LoginButton,
+  AccessToken,
+  LoginManager,
+  Profile,
+  GraphRequest,
+  GraphRequestManager,
+  FBAccessToken,
+} from 'react-native-fbsdk-next';
 
 import SafeAreaContainer from '../containers/SafeAreaContainer';
+import {AuthButton} from '../components/atoms/Button';
 
 type Props = NativeStackScreenProps<RootGuestStackParamList, 'Welcome'>;
 const WelcomeScreen = ({navigation}: Props) => {
+  const loginFb = () => {
+    LoginManager.logInWithPermissions([
+      'pages_show_list',
+      'instagram_basic',
+      'business_management',
+    ])
+      .then(
+        function (result) {
+          if (result.isCancelled) {
+            console.log('Login cancelled');
+          } else {
+            console.log(result);
+            console.log(
+              'Login success with permissions: ' +
+                result?.grantedPermissions?.toString(),
+            );
+          }
+        },
+        function (error) {
+          console.log('Login fail with error: ' + error);
+        },
+      )
+      .catch(err => console.log(err));
+  };
+
+  const instagramDataCallback = (error?: Object, result?: any) => {
+    if (error) {
+      console.log('Error fetching data: ' + error.toString());
+    } else {
+      console.log(result);
+    }
+  };
+  const userInstagramBusinessAccountCallback = (
+    error?: Object,
+    result?: any,
+  ) => {
+    if (error) {
+      console.log('Error fetching data: ' + error.toString());
+    } else {
+      const instagramBusinessAccount = result?.instagram_business_account;
+      const instagramId = instagramBusinessAccount?.id;
+      const infoRequest = new GraphRequest(
+        `/${instagramId}`,
+        {
+          parameters: {
+            fields: {
+              string:
+                'id,followers_count,media_count,username,website,biography',
+            },
+          },
+        },
+        instagramDataCallback,
+      );
+      new GraphRequestManager().addRequest(infoRequest).start();
+    }
+  };
+  const userPagesCallback = (error?: Object, result?: any) => {
+    if (error) {
+      console.log('Error fetching data: ' + error.toString());
+    } else {
+      const data = result?.data;
+      if (data && data?.length > 0) {
+        const page = data?.[0];
+        if (page) {
+          const page_id = page?.id;
+          const infoRequest = new GraphRequest(
+            `/${page_id}`,
+            {
+              parameters: {
+                fields: {
+                  string: 'instagram_business_account',
+                },
+              },
+            },
+            userInstagramBusinessAccountCallback,
+          );
+          new GraphRequestManager().addRequest(infoRequest).start();
+        }
+      }
+      console.log(result);
+    }
+  };
+  AccessToken.getCurrentAccessToken()
+    .then((fbAccessToken: any) => {
+      if (fbAccessToken?.accessToken) {
+        const infoRequest = new GraphRequest(
+          '/me/accounts',
+          {},
+          userPagesCallback,
+        );
+        // Start the graph request.
+        new GraphRequestManager().addRequest(infoRequest).start();
+      }
+    })
+    .catch(err => console.log('access token error', err));
+
+  // Create a graph request asking for user information with a callback to handle the response.
+
   return (
     <SafeAreaContainer>
       <View className="h-full w-full bg-white flex flex-col justify-between items-center">
@@ -23,6 +131,11 @@ const WelcomeScreen = ({navigation}: Props) => {
           </View>
           <View className="w-full justify-between items-center px-5 py-7">
             <View className="w-full flex flex-col">
+              <AuthButton
+                text="Login with Facebook"
+                rounded="default"
+                onPress={loginFb}
+              />
               <Button
                 title="Sign In"
                 buttonStyle={{
