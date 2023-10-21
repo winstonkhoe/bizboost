4f7421b73318ebf203e7731ffae3f4d906ca948c
Winston <winstonkhoe@yahoo.com>
2023-10-21 22:40:28 +0700

    feat: new component to normalize back button page

---
+++ src/components/templates/PageWithBackButton.tsx
@@ -0,0 +1,34 @@
+import {ReactNode} from 'react';
+import SafeAreaContainer from '../../containers/SafeAreaContainer';
+import {HorizontalPadding} from '../atoms/ViewPadding';
+import {flex} from '../../styles/Flex';
+import {ScrollView} from 'react-native-gesture-handler';
+import {BackButtonPlaceholder} from '../molecules/BackButtonPlaceholder';
+import {Text, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+interface Props {
+  children: ReactNode;
+  fullHeight?: boolean;
+}
+
+export const PageWithBackButton = ({children, fullHeight = false}: Props) => {
+  const navigation = useNavigation();
+  return (
+    <SafeAreaContainer>
+      <ScrollView
+        contentContainerStyle={fullHeight && {flexGrow: 1}}
+        className="h-full w-full"
+        style={[flex.flexCol]}>
+        <HorizontalPadding>
+          <BackButtonPlaceholder
+            onPress={() => {
+              navigation.goBack();
+            }}
+          />
+        </HorizontalPadding>
+        {children}
+      </ScrollView>
+    </SafeAreaContainer>
+  );
+};
