4aa324922f027caaa8f82e214388daec3e580e27
Winston <winstonkhoe@yahoo.com>
2023-10-21 21:17:21 +0700

    refactor: button vertical padding to props

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -6,24 +6,28 @@ import {background} from '../../styles/BackgroundColor';
 import {COLOR} from '../../styles/Color';
 import {textColor} from '../../styles/Text';
 import {border} from '../../styles/Border';
+import {PaddingSizeType, verticalPadding} from '../../styles/Padding';
 
 interface Props extends PressableProps, React.RefAttributes<View> {
   text: string;
   rounded?: RadiusSizeType;
   inverted?: boolean;
+  verticalPadding?: PaddingSizeType;
 }
 export const CustomButton = ({
   text,
   rounded: roundSize = 'default',
+  verticalPadding: verticalPaddingSize = 'small',
   inverted = false,
   ...props
 }: Props) => {
   return (
     <Pressable
       {...props}
-      className="w-full justify-center items-center text-center px-6 py-3"
+      className="w-full justify-center items-center text-center px-6"
       style={[
         flex.flexRow,
+        verticalPadding[verticalPaddingSize],
         rounded[roundSize],
         inverted &&
           border({
