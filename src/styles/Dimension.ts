a2b1d764748854193566e2309304ec5cfff50f9e
Winston <winstonkhoe@yahoo.com>
2023-10-28 22:50:05 +0700

    feat(style-dimension): init dimension style
    for width and height utils

---
+++ src/styles/Dimension.ts
@@ -0,0 +1,52 @@
+import {StyleSheet, ViewStyle} from 'react-native';
+import {SizeStyle, SizeType, size} from './Size';
+
+export const dimension = {
+  square: {
+    ...StyleSheet.create<SizeStyle>(
+      Object.entries(size).reduce((acc, [key, value]) => {
+        return {
+          ...acc,
+          [key]: {
+            width: value,
+            height: value,
+          } as ViewStyle,
+        };
+      }, {} as Record<keyof SizeType, ViewStyle>),
+    ),
+    full: {
+      width: '100%',
+      height: '100%',
+    } as ViewStyle,
+  },
+  width: {
+    ...StyleSheet.create<SizeStyle>(
+      Object.entries(size).reduce((acc, [key, value]) => {
+        return {
+          ...acc,
+          [key]: {
+            width: value,
+          } as ViewStyle,
+        };
+      }, {} as Record<keyof SizeType, ViewStyle>),
+    ),
+    full: {
+      width: '100%',
+    } as ViewStyle,
+  },
+  height: {
+    ...StyleSheet.create<SizeStyle>(
+      Object.entries(size).reduce((acc, [key, value]) => {
+        return {
+          ...acc,
+          [key]: {
+            height: value,
+          } as ViewStyle,
+        };
+      }, {} as Record<keyof SizeType, ViewStyle>),
+    ),
+    full: {
+      height: '100%',
+    } as ViewStyle,
+  },
+};
