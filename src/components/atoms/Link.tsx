492b4b495be325ae30c9b9432fb890a324919c25
Winston <winstonkhoe@yahoo.com>
2023-10-31 00:15:44 +0700

    feat(link): add pressable and style normalization

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -1,10 +1,22 @@
-import {Text} from 'react-native';
+import {Pressable, PressableProps, Text} from 'react-native';
+import {textColor} from '../../styles/Text';
+import {COLOR} from '../../styles/Color';
+import {FontSizeType, font} from '../../styles/Font';
 
-interface Props {
+interface Props extends PressableProps {
   text: string;
+  size?: FontSizeType;
 }
-const InternalLink = ({text}: Props) => {
-  return <Text className="text-blue-700 font-semibold text-sm">{text}</Text>;
+const InternalLink = ({text, size: sizeType = 40, ...props}: Props) => {
+  return (
+    <Pressable {...props}>
+      <Text
+        className="font-bold"
+        style={[textColor(COLOR.green[60]), font.size[sizeType]]}>
+        {text}
+      </Text>
+    </Pressable>
+  );
 };
 
 export {InternalLink};
