ac56f8a56754af3a7f5bfca452098e06c87e3d37
Winston <winstonkhoe@yahoo.com>
2023-11-03 00:17:49 +0700

    feat(image-card): able to dim images

--- src/components/molecules/ImageCard.tsx
+++ src/components/molecules/ImageCard.tsx
@@ -5,11 +5,13 @@ import {textColor} from '../../styles/Text';
 import {COLOR} from '../../styles/Color';
 import {padding} from '../../styles/Padding';
 import LinearGradient from 'react-native-linear-gradient';
+import { background } from '../../styles/BackgroundColor';
 
 interface SimpleImageCardProps {
   image: string;
   width?: SizeType | 'full';
   height?: SizeType | 'full';
+  dim?: 0 | 11 | 22 | 33 | 44 | 55 | 66 | 77 | 88;
   text: string;
 }
 
@@ -18,17 +20,27 @@ export const SimpleImageCard = ({
   text,
   width = 'xlarge6',
   height = 'xlarge3',
+  dim = 0,
 }: SimpleImageCardProps) => {
   return (
-    <View style={[dimension.width[width], dimension.height[height]]}>
+    <View
+      className="relative"
+      style={[dimension.width[width], dimension.height[height]]}>
       <Image
         style={[dimension.full]}
         source={{
           uri: image,
         }}
       />
+      <View
+        className="absolute z-10 top-0 left-0"
+        style={[
+          dimension.full,
+          background(`${COLOR.background.neutral.high}${dim}`),
+        ]}
+      />
       <LinearGradient
-        className="absolute bottom-0 left-0"
+        className="absolute z-20 bottom-0 left-0"
         colors={[`${COLOR.black[60]}11`, `${COLOR.black[100]}`]}
         style={[dimension.width.full, padding.small, padding.top.large]}>
         <Text className="font-bold" style={[textColor(COLOR.black[0])]}>
