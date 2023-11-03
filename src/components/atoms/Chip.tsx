b55581d41d04d4a1cbe290d2c87d6010d067b706
Winston <winstonkhoe@yahoo.com>
2023-11-03 16:42:33 +0700

    feat(chip): add removable chip

--- src/components/atoms/Chip.tsx
+++ src/components/atoms/Chip.tsx
@@ -1,4 +1,4 @@
-import {View} from 'react-native';
+import {Pressable, PressableProps, View} from 'react-native';
 import {dimension} from '../../styles/Dimension';
 import {background} from '../../styles/BackgroundColor';
 import {COLOR} from '../../styles/Color';
@@ -16,6 +16,10 @@ import Animated, {
 } from 'react-native-reanimated';
 import {useEffect} from 'react';
 import {SizeType} from '../../styles/Size';
+import {gap} from '../../styles/Gap';
+import {AddIcon} from './Icon';
+import {padding} from '../../styles/Padding';
+import { AnimatedPressable } from './AnimatedPressable';
 
 interface ImageCounterChipProps {
   text?: string | number;
@@ -74,5 +78,39 @@ export const ImageCounterChip = ({
           ]}></View>
       )}
     </Animated.View>
+  );
+};
+
+interface RemovableChipProps extends PressableProps {
+  text?: string | number;
+}
+
+export const RemovableChip = ({text, ...props}: RemovableChipProps) => {
+  return (
+    <AnimatedPressable {...props}>
+      <View
+        style={[
+          flex.flexRow,
+          items.center,
+          gap.small,
+          rounded.max,
+          padding.vertical.small,
+          padding.horizontal.default,
+          background(COLOR.green[5]),
+          border({
+            borderWidth: 1,
+            color: COLOR.green[50],
+          }),
+        ]}>
+        <Text
+          className="font-semibold"
+          style={[textColor(COLOR.green[50]), font.size[40]]}>
+          {text}
+        </Text>
+        <View className="rotate-45">
+          <AddIcon color={COLOR.green[50]} />
+        </View>
+      </View>
+    </AnimatedPressable>
   );
 };
