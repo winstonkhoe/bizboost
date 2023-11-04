5c2bd0dd5e728d842465aa6278d046202adb67d8
Winston <winstonkhoe@yahoo.com>
2023-11-04 14:28:11 +0700

    feat(signup): massive update to signup process

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,5 +1,5 @@
 import {Alert, Text, View} from 'react-native';
-import {User} from '../model/User';
+import {User, UserRole, UserRoles} from '../model/User';
 import {useForm, FormProvider} from 'react-hook-form';
 import {CustomTextInput} from '../components/atoms/Input';
 import {
@@ -12,8 +12,27 @@ import {textColor} from '../styles/Text';
 import {COLOR} from '../styles/Color';
 import {PageWithBackButton} from '../components/templates/PageWithBackButton';
 import {CustomButton} from '../components/atoms/Button';
-import {allowAccess} from '../redux/slices/authSlice';
-import {useAppDispatch} from '../redux/hooks';
+import {useAppSelector} from '../redux/hooks';
+import PagerView from 'react-native-pager-view';
+import {isValidField} from '../utils/form';
+import {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import {verticalPadding} from '../styles/Padding';
+import {Stepper} from '../components/atoms/Stepper';
+import {Provider} from '../model/AuthMethod';
+import {KeyboardAvoidingContainer} from '../containers/KeyboardAvoidingContainer';
+import {useNavigation} from '@react-navigation/native';
+import {NavigationStackProps} from '../navigation/StackNavigation';
+import {Location} from '../model/Location';
+import {openLocationModal} from '../utils/modal';
+import {Category} from '../model/Category';
+import {ChooseRole} from './signup/ChooseRole';
+import {
+  PlatformData,
+  RegisterSocialPlatform,
+} from './signup/RegisterSocialPlatform';
+import {RegisterFocusCategory} from './signup/RegisterFocusCategory';
+import {RegisterLocation} from './signup/RegisterLocation';
+import { RegisterContentCreatorPreferences } from './signup/RegisterContentCreatorPreferences';
 
 type FormData = {
   email: string;
@@ -22,14 +41,74 @@ type FormData = {
   fullname: string;
   phone: string;
 };
+
+enum SignupStep {
+  EMAIL = 0,
+  PASSWORD = 1,
+  ROLE = 2,
+  NAME_PHONE = 3,
+  SOCIAL_PLATFORM = 4,
+  FAVORITE_CATEGORY = 5,
+  LOCATION = 6,
+  CONTENT_CREATOR_PREFERENCES = 7,
+  PROFILE_PICTURE = 8,
+}
+
 const SignUpScreen = () => {
-  const dispatch = useAppDispatch();
+  const [platformDatas, setPlatformDatas] = useState<PlatformData[]>([]);
+  const [isValidPlatformData, setIsValidPlatformData] =
+    useState<boolean>(false);
+  const [focusCategories, setFocusCategories] = useState<Category[]>([]);
+  const [preferredLocations, setPreferredLocations] = useState<Location[]>([]);
+  const generalNavigation = useNavigation<NavigationStackProps>();
+  const {
+    temporaryData: temporaryUserSignupData,
+    data: userSignupData,
+    provider,
+    role,
+  } = useAppSelector(select => select.signup);
+  const [currentRole, setCurrentRole] = useState<UserRoles | undefined>(
+    undefined,
+  );
+  const [activePosition, setActivePosition] = useState<number>(0);
+  const [actionText, setActionText] = useState<string>('Next');
+  const steps = useMemo(() => {
+    let commonSteps = [SignupStep.ROLE, SignupStep.NAME_PHONE];
+    if (Provider.GOOGLE === provider) {
+      if (!userSignupData?.email) {
+        commonSteps = [SignupStep.EMAIL, ...commonSteps];
+      }
+    } else if (Provider.FACEBOOK === provider) {
+      commonSteps = [SignupStep.EMAIL, ...commonSteps];
+    } else {
+      commonSteps = [SignupStep.EMAIL, SignupStep.PASSWORD, ...commonSteps];
+    }
+
+    if (UserRole.ContentCreator === role) {
+      commonSteps = [
+        ...commonSteps,
+        SignupStep.SOCIAL_PLATFORM,
+        SignupStep.FAVORITE_CATEGORY,
+        SignupStep.LOCATION,
+        SignupStep.CONTENT_CREATOR_PREFERENCES,
+      ];
+    }
+
+    commonSteps = [
+      SignupStep.CONTENT_CREATOR_PREFERENCES,
+      ...commonSteps,
+      SignupStep.PROFILE_PICTURE,
+    ];
+    return commonSteps;
+  }, [provider, userSignupData, role]);
+
+  const pagerViewRef = useRef<PagerView>(null);
   const methods = useForm<FormData>({
     mode: 'all',
     defaultValues: {},
   });
 
-  const {handleSubmit, watch} = methods;
+  const {handleSubmit, getFieldState, watch, formState, setValue} = methods;
 
   const onSubmit = (data: FormData) => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -53,123 +132,274 @@ const SignUpScreen = () => {
     });
   };
 
-  const handleSignupWithGoogle = () => {
-    User.signUpWithGoogle();
+  const hasNext = useCallback(() => {
+    return activePosition + 1 < steps.length;
+  }, [activePosition, steps]);
+
+  const nextPage = () => {
+    if (hasNext()) {
+      pagerViewRef.current?.setPage(steps[activePosition + 1]);
+    } else {
+      handleSubmit(onSubmit);
+    }
   };
+
+  const previousPage = () => {
+    pagerViewRef.current?.setPage(steps[activePosition - 1]);
+  };
+
+  const setFieldValue = useCallback(
+    (field: keyof FormData, data: any) => {
+      data &&
+        setValue(field, data, {
+          shouldValidate: true,
+          shouldTouch: true,
+          shouldDirty: true,
+        });
+    },
+    [setValue],
+  );
+
+  useEffect(() => {
+    if (steps.includes(SignupStep.EMAIL)) {
+      setFieldValue('email', userSignupData?.email);
+    }
+
+    if (steps.includes(SignupStep.NAME_PHONE)) {
+      setFieldValue('fullname', temporaryUserSignupData?.fullname);
+    }
+  }, [steps, setFieldValue, userSignupData, temporaryUserSignupData]);
+
+  useEffect(() => {
+    if (hasNext()) {
+      setActionText('Next');
+    } else {
+      setActionText('Create Account');
+    }
+  }, [activePosition, steps.length, hasNext]);
+
   return (
-    <PageWithBackButton>
-      <FormProvider {...methods}>
-        <VerticalPadding paddingSize="large">
+    <FormProvider {...methods}>
+      <PageWithBackButton
+        fullHeight
+        onPress={previousPage}
+        backButtonPlaceholder={
+          <View className="justify-center" style={[flex.flexRow]}>
+            <Text
+              className="text-lg font-semibold"
+              style={[textColor(COLOR.text.neutral.high)]}>
+              Sign Up
+            </Text>
+          </View>
+        }
+        disableDefaultOnPress={activePosition > 0}>
+        <View
+          className="flex-1"
+          style={[flex.flexCol, verticalPadding.default]}>
           <HorizontalPadding paddingSize="large">
-            <View
-              className="h-full items-center justify-center"
-              style={[flex.flexCol, gap.medium]}>
-              {/* App Bar */}
-              <Text
-                className="text-2xl font-bold"
-                style={[textColor(COLOR.text.neutral.high)]}>
-                Sign Up
-              </Text>
+            <VerticalPadding>
+              <Stepper
+                currentPosition={activePosition}
+                maxPosition={steps.length}
+              />
+            </VerticalPadding>
+          </HorizontalPadding>
+          <PagerView
+            ref={pagerViewRef}
+            className="flex-1"
+            initialPage={steps[activePosition]}
+            scrollEnabled={false}
+            onPageSelected={e => {
+              const position = e.nativeEvent.position;
+              console.log(e.nativeEvent);
+              console.log('position', position);
 
-              {/* Content */}
-              <View className="w-full" style={[flex.flexCol, gap.small]}>
-                <CustomTextInput
-                  label="Full name"
-                  name="fullname"
-                  rules={{
-                    required: 'Fullname is required',
-                  }}
+              setActivePosition(steps.findIndex(step => step === position));
+            }}>
+            <View key={SignupStep.EMAIL}>
+              <VerticalPadding paddingSize="large">
+                <HorizontalPadding paddingSize="large">
+                  <View style={[flex.flexCol, gap.xlarge]}>
+                    <CustomTextInput
+                      label="Email"
+                      name="email"
+                      rules={{
+                        required: 'Email is required',
+                        pattern: {
+                          value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+                          message: 'Email address must be a valid address',
+                        },
+                      }}
+                    />
+                    <CustomButton
+                      text={actionText}
+                      rounded="max"
+                      minimumWidth
+                      disabled={
+                        !isValidField(getFieldState('email', formState))
+                      }
+                      onPress={nextPage}
+                    />
+                  </View>
+                </HorizontalPadding>
+              </VerticalPadding>
+            </View>
+            <View key={SignupStep.PASSWORD}>
+              <VerticalPadding paddingSize="large">
+                <HorizontalPadding paddingSize="large">
+                  <View style={[flex.flexCol, gap.xlarge]}>
+                    <CustomTextInput
+                      label="Password"
+                      name="password"
+                      hideInputText
+                      rules={{
+                        required: 'Password is required',
+                        minLength: {
+                          value: 8,
+                          message: 'Password must be at least 8 characters',
+                        },
+                      }}
+                    />
+
+                    <CustomTextInput
+                      label="Confirm password"
+                      name="confirmPassword"
+                      hideInputText
+                      rules={{
+                        required: 'Confirm Password must be filled',
+                        validate: value =>
+                          value === watch('password') ||
+                          'Confirm password must be the same as password',
+                      }}
+                    />
+                    <CustomButton
+                      text={actionText}
+                      rounded="max"
+                      minimumWidth
+                      disabled={
+                        !isValidField(getFieldState('password', formState)) ||
+                        !isValidField(
+                          getFieldState('confirmPassword', formState),
+                        )
+                      }
+                      onPress={nextPage}
+                    />
+                  </View>
+                </HorizontalPadding>
+              </VerticalPadding>
+            </View>
+            <View key={SignupStep.ROLE}>
+              <View className="items-center" style={[flex.flexCol, gap.small]}>
+                <ChooseRole onChangeRole={setCurrentRole} />
+                <CustomButton
+                  text={actionText}
+                  rounded="max"
+                  minimumWidth
+                  onPress={nextPage}
                 />
-                <CustomTextInput
-                  keyboardType="phone-pad"
-                  label="Phone Number"
-                  name="phone"
-                  rules={{
-                    required: 'Phone number is required',
-                    pattern: {
-                      value: /^(^\+62|62|^08)(\d{3,4}-?){2}\d{3,4}$/g,
-                      message: 'Phone number is invalid',
-                    },
-                  }}
-                />
-                <CustomTextInput
-                  label="Email"
-                  name="email"
-                  rules={{
-                    required: 'Email is required',
-                    pattern: {
-                      value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-                      message: 'Email address must be a valid address',
-                    },
-                  }}
-                />
-
-                <CustomTextInput
-                  label="Password"
-                  name="password"
-                  hideInputText
-                  rules={{
-                    required: 'Password is required',
-                    minLength: {
-                      value: 8,
-                      message: 'Password must be at least 8 characters',
-                    },
-                  }}
-                />
-
-                <CustomTextInput
-                  label="Confirm password"
-                  name="confirmPassword"
-                  hideInputText
-                  rules={{
-                    required: 'Confirm Password must be filled',
-                    validate: value =>
-                      value === watch('password') ||
-                      'Confirm password must be the same as password',
-                  }}
-                />
-              </View>
-
-              {/* Login Button */}
-              <View className="w-full pt-5" style={[flex.flexCol]}>
-                <View className="w-full" style={[flex.flexCol, gap.default]}>
-                  <CustomButton
-                    text="Sign Up"
-                    rounded="max"
-                    onPress={handleSubmit(onSubmit)}
-                  />
-
-                  <CustomButton
-                    text="Continue with Google"
-                    rounded="max"
-                    inverted
-                    onPress={handleSignupWithGoogle}
-                  />
-
-                  <CustomButton
-                    text="Continue with Facebook"
-                    rounded="max"
-                    inverted
-                    onPress={() =>
-                      User.signUpWithFacebook(res => {
-                        if (res) {
-                          console.log('success from welcome screen');
-                          dispatch(allowAccess());
-                        } else {
-                          console.log('failed');
-                        }
-                      })
-                        .then(() => console.log('done then'))
-                        .catch(err => console.log('err nih', err))
-                    }
-                  />
-                </View>
               </View>
             </View>
-          </HorizontalPadding>
-        </VerticalPadding>
-      </FormProvider>
-    </PageWithBackButton>
+            <View key={SignupStep.NAME_PHONE}>
+              <VerticalPadding paddingSize="large">
+                <HorizontalPadding paddingSize="large">
+                  <View style={[flex.flexCol, gap.xlarge]}>
+                    <CustomTextInput
+                      label="Full name"
+                      name="fullname"
+                      rules={{
+                        required: 'Fullname is required',
+                      }}
+                    />
+                    <CustomTextInput
+                      keyboardType="phone-pad"
+                      label="Phone Number"
+                      name="phone"
+                      rules={{
+                        required: 'Phone number is required',
+                        pattern: {
+                          value: /^(^\+62|62|^08)(\d{3,4}-?){2}\d{3,4}$/g,
+                          message: 'Phone number is invalid',
+                        },
+                      }}
+                    />
+                    <CustomButton
+                      text={actionText}
+                      rounded="max"
+                      minimumWidth
+                      disabled={
+                        !isValidField(getFieldState('fullname', formState)) ||
+                        !isValidField(getFieldState('phone', formState))
+                      }
+                      onPress={nextPage}
+                    />
+                  </View>
+                </HorizontalPadding>
+              </VerticalPadding>
+            </View>
+            <View key={SignupStep.SOCIAL_PLATFORM}>
+              <KeyboardAvoidingContainer>
+                <View style={[flex.flexCol, gap.xlarge3]}>
+                  <RegisterSocialPlatform
+                    onChangeSocialData={setPlatformDatas}
+                    onValidRegistration={setIsValidPlatformData}
+                  />
+                  <CustomButton
+                    text={platformDatas.length === 0 ? 'Skip' : actionText}
+                    rounded="max"
+                    minimumWidth
+                    disabled={!isValidPlatformData}
+                    onPress={nextPage}
+                  />
+                </View>
+              </KeyboardAvoidingContainer>
+            </View>
+            <View key={SignupStep.FAVORITE_CATEGORY}>
+              <KeyboardAvoidingContainer>
+                <View style={[flex.flexCol, gap.xlarge2]}>
+                  <RegisterFocusCategory
+                    onCategoriesChange={setFocusCategories}
+                  />
+                  <CustomButton
+                    text={focusCategories.length === 0 ? 'Skip' : actionText}
+                    rounded="max"
+                    minimumWidth
+                    onPress={nextPage}
+                  />
+                </View>
+              </KeyboardAvoidingContainer>
+            </View>
+            <View key={SignupStep.LOCATION}>
+              <KeyboardAvoidingContainer>
+                <View style={[flex.flexCol, gap.xlarge2]}>
+                  <RegisterLocation onLocationsChange={setPreferredLocations} />
+                  <CustomButton
+                    text={preferredLocations.length === 0 ? 'Skip' : actionText}
+                    rounded="max"
+                    minimumWidth
+                    onPress={nextPage}
+                  />
+                </View>
+              </KeyboardAvoidingContainer>
+            </View>
+            <View key={SignupStep.CONTENT_CREATOR_PREFERENCES}>
+              <KeyboardAvoidingContainer>
+                <View style={[flex.flexCol, gap.xlarge2]}>
+                  <RegisterContentCreatorPreferences
+                    onLocationsChange={setPreferredLocations}
+                  />
+                  <CustomButton
+                    text={preferredLocations.length === 0 ? 'Skip' : actionText}
+                    rounded="max"
+                    minimumWidth
+                    onPress={nextPage}
+                  />
+                </View>
+              </KeyboardAvoidingContainer>
+            </View>
+          </PagerView>
+        </View>
+      </PageWithBackButton>
+    </FormProvider>
   );
 };
 
