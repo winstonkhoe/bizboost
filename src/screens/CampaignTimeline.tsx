8cdb6df30928d095d86ce3d503f8b978146060d9
Winston <winstonkhoe@yahoo.com>
2023-11-10 23:15:45 +0700

    feat(campaign-timeline): implement content stepper

--- src/screens/CampaignTimeline.tsx
+++ src/screens/CampaignTimeline.tsx
@@ -15,6 +15,16 @@ import {PageWithBackButton} from '../components/templates/PageWithBackButton';
 import {useNavigation} from '@react-navigation/native';
 import {flex} from '../styles/Flex';
 import {gap} from '../styles/Gap';
+import {Stepper} from '../components/atoms/Stepper';
+import {font} from '../styles/Font';
+import {HorizontalPadding} from '../components/atoms/ViewPadding';
+import {padding} from '../styles/Padding';
+import {rounded} from '../styles/BorderRadius';
+import {background} from '../styles/BackgroundColor';
+import {COLOR} from '../styles/Color';
+import {CustomButton} from '../components/atoms/Button';
+import {border} from '../styles/Border';
+import {textColor} from '../styles/Text';
 
 type Props = NativeStackScreenProps<
   AuthenticatedStack,
@@ -36,10 +46,67 @@ const CampaignTimelineScreen = ({route}: Props) => {
   }
 
   return (
-    <PageWithBackButton>
-      <View style={[flex.flexCol, gap.default]}>
-        
-      </View>
+    <PageWithBackButton enableSafeAreaContainer fullHeight>
+      <HorizontalPadding>
+        <View style={[flex.flexCol, gap.default, padding.top.xlarge3]}>
+          <Stepper type="content" currentPosition={1} maxPosition={6}>
+            {[...Array(5)].map((_, i) => (
+              <View key={i} style={[flex.flexCol, gap.default]}>
+                <View style={[flex.flexCol]}>
+                  <Text
+                    className="font-bold"
+                    style={[font.size[60]]}
+                    numberOfLines={1}>
+                    Registration
+                  </Text>
+                  <Text
+                    style={[font.size[30], textColor(COLOR.text.neutral.high)]}
+                    numberOfLines={1}>
+                    24 Oct 2023 - 28 Oct 2023
+                  </Text>
+                </View>
+                <View
+                  style={[
+                    flex.flexCol,
+                    padding.default,
+                    rounded.default,
+                    gap.default,
+                    border({
+                      borderWidth: 3,
+                      color: COLOR.green[60],
+                      opacity: 0.5,
+                    }),
+                  ]}>
+                  <Text
+                    className="font-semibold"
+                    style={[font.size[60]]}
+                    numberOfLines={1}>
+                    Submit your ideas!
+                  </Text>
+                  <View
+                    style={[
+                      flex.flexCol,
+                      gap.default,
+                      padding.default,
+                      rounded.default,
+                      background(`${COLOR.green[5]}`),
+                    ]}>
+                    <Text>💡 Things to highlight</Text>
+                    <Text>
+                      Features: Multiple compartments for organized packing
+                      Water-resistant and weather-proof material Lightweight and
+                      easy to carry Available in various sizes and colors
+                      Tagline: “Travel with confidence with Koper Idaman
+                      Petualang!”
+                    </Text>
+                  </View>
+                  <CustomButton text="Submit idea" />
+                </View>
+              </View>
+            ))}
+          </Stepper>
+        </View>
+      </HorizontalPadding>
     </PageWithBackButton>
   );
 };
