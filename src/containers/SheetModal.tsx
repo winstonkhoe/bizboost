47c2cd3d37fcb59b7c6355c2f8944186f6fc58d4
Winston <winstonkhoe@yahoo.com>
2023-11-12 00:52:11 +0700

    feat(sheet-modal): add max height calculation

--- src/containers/SheetModal.tsx
+++ src/containers/SheetModal.tsx
@@ -6,19 +6,25 @@ import {
 import {ReactNode, useCallback, useEffect, useRef} from 'react';
 import {background} from '../styles/BackgroundColor';
 import {COLOR} from '../styles/Color';
+import {useWindowDimensions} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 interface SheetModalProps {
   open: boolean;
   children: ReactNode;
+  maxHeight?: number;
   onDismiss?: () => void;
 }
 
 export const SheetModal = ({
   open = false,
   onDismiss,
+  maxHeight,
   children,
 }: SheetModalProps) => {
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const windowDimensions = useWindowDimensions();
+  const safeAreaInsets = useSafeAreaInsets();
 
   const renderBackdrop = useCallback(
     (props: any) => (
@@ -45,6 +51,11 @@ export const SheetModal = ({
       ref={bottomSheetModalRef}
       onDismiss={onDismiss}
       backdropComponent={renderBackdrop}
+      maxDynamicContentSize={
+        maxHeight
+          ? maxHeight
+          : windowDimensions.height - (safeAreaInsets.top + 30)
+      }
       enableDynamicSizing
       enablePanDownToClose>
       <BottomSheetView>{children}</BottomSheetView>
