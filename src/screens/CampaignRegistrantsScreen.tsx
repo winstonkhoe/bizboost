425426c719e99e5052f80c57c2c85c33c2745ec6
je-von <jevon.levin@gmail.com>
2023-11-12 15:09:45 +0700

    fix: spacing add CampaignRegistrantScreen

--- src/screens/CampaignRegistrantsScreen.tsx
+++ src/screens/CampaignRegistrantsScreen.tsx
@@ -1,4 +1,4 @@
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import {CloseModal} from '../components/atoms/Close';
 import SafeAreaContainer from '../containers/SafeAreaContainer';
 import {HorizontalPadding} from '../components/atoms/ViewPadding';
@@ -10,6 +10,8 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {useEffect, useState} from 'react';
 import {Transaction} from '../model/Transaction';
 import RegisteredUserListCard from '../components/molecules/RegisteredUserListCard';
+import {flex} from '../styles/Flex';
+import {gap} from '../styles/Gap';
 type Props = NativeStackScreenProps<
   AuthenticatedStack,
   AuthenticatedNavigation.CampaignRegistrants
@@ -27,14 +29,16 @@ const CampaignRegistrantsScreen = ({route}: Props) => {
     <SafeAreaContainer>
       <CloseModal />
       <HorizontalPadding>
-        <Text className="text-lg font-bold">Registrants</Text>
-        {transactions.length <= 0 ? (
-          <Text>No Content Creator has joined this campaign!</Text>
-        ) : (
-          transactions.map((t, index) => (
-            <RegisteredUserListCard transaction={t} key={index} />
-          ))
-        )}
+        <View style={[flex.flexCol, gap.medium]}>
+          <Text className="text-lg font-bold">Registrants</Text>
+          {transactions.length <= 0 ? (
+            <Text>No Content Creator has joined this campaign!</Text>
+          ) : (
+            transactions.map((t, index) => (
+              <RegisteredUserListCard transaction={t} key={index} />
+            ))
+          )}
+        </View>
       </HorizontalPadding>
     </SafeAreaContainer>
   );
