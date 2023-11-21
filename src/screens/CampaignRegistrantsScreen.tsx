56d8619c7b94447e6657232420eceb9fbc1bab6b
Winston <winstonkhoe@yahoo.com>
2023-11-21 22:58:33 +0700

    feat(campaign-registrant): add filter by status

--- src/screens/CampaignRegistrantsScreen.tsx
+++ src/screens/CampaignRegistrantsScreen.tsx
@@ -1,24 +1,33 @@
 import {Text, View} from 'react-native';
-import {CloseModal} from '../components/atoms/Close';
-import SafeAreaContainer from '../containers/SafeAreaContainer';
-import {HorizontalPadding} from '../components/atoms/ViewPadding';
 import {
   AuthenticatedNavigation,
   AuthenticatedStack,
 } from '../navigation/StackNavigation';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {useEffect, useState} from 'react';
-import {Transaction} from '../model/Transaction';
+import {Transaction, TransactionStatus} from '../model/Transaction';
 import RegisteredUserListCard from '../components/molecules/RegisteredUserListCard';
 import {flex} from '../styles/Flex';
 import {gap} from '../styles/Gap';
 import {UserRole} from '../model/User';
+import {PageWithBackButton} from '../components/templates/PageWithBackButton';
+import {padding} from '../styles/Padding';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {size} from '../styles/Size';
+import {ScrollView} from 'react-native-gesture-handler';
+import SelectableTag from '../components/atoms/SelectableTag';
+import {COLOR} from '../styles/Color';
+import {background} from '../styles/BackgroundColor';
 type Props = NativeStackScreenProps<
   AuthenticatedStack,
   AuthenticatedNavigation.CampaignRegistrants
 >;
 const CampaignRegistrantsScreen = ({route}: Props) => {
-  const {campaignId} = route.params;
+  const {campaignId, initialTransactionStatusFilter} = route.params;
+  const safeAreaInsets = useSafeAreaInsets();
+  const [statusFilter, setStatusFilter] = useState<
+    TransactionStatus | undefined
+  >(initialTransactionStatusFilter);
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   useEffect(() => {
@@ -26,26 +35,99 @@ const CampaignRegistrantsScreen = ({route}: Props) => {
       setTransactions(t),
     );
   }, [campaignId]);
+
+  const updateStatusFilter = (status: TransactionStatus | undefined) => {
+    if (statusFilter !== status) {
+      setStatusFilter(status);
+    }
+  };
+
   return (
-    <SafeAreaContainer enable>
-      <CloseModal />
-      <HorizontalPadding>
-        <View style={[flex.flexCol, gap.medium]}>
+    <PageWithBackButton
+      fullHeight
+      icon="close"
+      backButtonPlaceholder={
+        <View style={[flex.flex1, flex.flexCol]}>
           <Text className="text-lg font-bold">Registrants</Text>
-          {transactions.length <= 0 ? (
-            <Text>No Content Creator has joined this campaign!</Text>
-          ) : (
-            transactions.map((t, index) => (
+        </View>
+      }
+      underBackButtonPlaceholder={
+        <View
+          style={[
+            background(COLOR.background.neutral.default),
+            padding.bottom.default,
+          ]}>
+          <ScrollView
+            showsHorizontalScrollIndicator={false}
+            style={[flex.flex1]}
+            horizontal
+            contentContainerStyle={[
+              padding.horizontal.default,
+              flex.grow,
+              flex.flexRow,
+              gap.small,
+            ]}>
+            <SelectableTag
+              text={'All'}
+              isSelected={statusFilter === undefined}
+              onPress={() => {
+                updateStatusFilter(undefined);
+              }}
+            />
+            <SelectableTag
+              text={TransactionStatus.registrationPending}
+              isSelected={
+                statusFilter === TransactionStatus.registrationPending
+              }
+              onPress={() => {
+                updateStatusFilter(TransactionStatus.registrationPending);
+              }}
+            />
+            <SelectableTag
+              text={TransactionStatus.offering}
+              isSelected={statusFilter === TransactionStatus.offering}
+              onPress={() => {
+                updateStatusFilter(TransactionStatus.offering);
+              }}
+            />
+            <SelectableTag
+              text={TransactionStatus.brainstormSubmitted}
+              isSelected={
+                statusFilter === TransactionStatus.brainstormSubmitted
+              }
+              onPress={() => {
+                updateStatusFilter(TransactionStatus.brainstormSubmitted);
+              }}
+            />
+          </ScrollView>
+        </View>
+      }>
+      <View
+        style={[
+          flex.flex1,
+          flex.flexCol,
+          gap.medium,
+          padding.horizontal.default,
+          background(COLOR.background.neutral.default),
+          {
+            paddingTop: safeAreaInsets.top + size.xlarge5,
+          },
+        ]}>
+        {transactions.length <= 0 ? (
+          <Text>No Content Creator has joined this campaign!</Text>
+        ) : (
+          transactions
+            .filter(t => (!statusFilter ? true : t.status === statusFilter))
+            .map((t, index) => (
               <RegisteredUserListCard
                 transaction={t}
                 role={UserRole.BusinessPeople}
                 key={index}
               />
             ))
-          )}
-        </View>
-      </HorizontalPadding>
-    </SafeAreaContainer>
+        )}
+      </View>
+    </PageWithBackButton>
   );
 };
 
