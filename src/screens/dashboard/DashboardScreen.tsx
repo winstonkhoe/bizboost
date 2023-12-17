52ef47645c265cc71b54d0081ce82b61fc1e108a
winstonkhoe <winstonkhoe@yahoo.com>
2023-12-18 03:20:49 +0700

    feat(dashboard): implement transaction filter
    action needed, terminated

--- src/screens/dashboard/DashboardScreen.tsx
+++ src/screens/dashboard/DashboardScreen.tsx
@@ -1,9 +1,14 @@
-import {ReactNode, useEffect, useState} from 'react';
+import {ReactNode, useEffect, useMemo, useState} from 'react';
 import {LoadingScreen} from '../LoadingScreen';
 import {Transaction} from '../../model/Transaction';
 import {useUser} from '../../hooks/user';
 import {UserRole} from '../../model/User';
-import {Pressable, View, useWindowDimensions} from 'react-native';
+import {
+  Pressable,
+  PressableProps,
+  View,
+  useWindowDimensions,
+} from 'react-native';
 import {flex, items, justify, self} from '../../styles/Flex';
 import {padding} from '../../styles/Padding';
 import {rounded} from '../../styles/BorderRadius';
@@ -30,12 +35,64 @@ import {
   AuthenticatedNavigation,
   NavigationStackProps,
 } from '../../navigation/StackNavigation';
+import {border} from '../../styles/Border';
+import RegisteredUserListCard from '../../components/molecules/RegisteredUserListCard';
+import {EmptyPlaceholder} from '../../components/templates/EmptyPlaceholder';
+
+enum FilterCardType {
+  ActionNeeded = 'Action Needed',
+  Terminated = 'Terminated',
+}
 
 const DashboardScreen = () => {
   const safeAreaInsets = useSafeAreaInsets();
   const {uid, user} = useUser();
+  const [activeFilterType, setActiveFilterType] = useState<FilterCardType>(
+    FilterCardType.ActionNeeded,
+  );
   const [isLoading, setIsLoading] = useState(false);
   const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [actionNeededTransactions, setActionNeededTransactions] =
+    useState<Transaction[]>();
+
+  const terminatedTransactions = useMemo(
+    () => transactions.filter(transaction => transaction.isTerminated()),
+    [transactions],
+  );
+
+  const filteredTransactions = useMemo(() => {
+    switch (activeFilterType) {
+      case FilterCardType.ActionNeeded:
+        return actionNeededTransactions;
+      case FilterCardType.Terminated:
+        return terminatedTransactions;
+      default:
+        return transactions;
+    }
+  }, [
+    activeFilterType,
+    actionNeededTransactions,
+    transactions,
+    terminatedTransactions,
+  ]);
+
+  useEffect(() => {
+    const getActionNeededTransactions = async () => {
+      const results = await Promise.all(
+        transactions.map(async transaction =>
+          (await transaction.isWaitingContentCreatorAction())
+            ? transaction
+            : null,
+        ),
+      );
+      return results.filter((result): result is Transaction => result !== null);
+    };
+    if (transactions) {
+      getActionNeededTransactions()
+        .then(setActionNeededTransactions)
+        .catch(() => setActionNeededTransactions([]));
+    }
+  }, [transactions]);
 
   useEffect(() => {
     if (uid) {
@@ -58,6 +115,7 @@ const DashboardScreen = () => {
           showsVerticalScrollIndicator={false}
           style={[flex.flex1]}
           contentContainerStyle={[
+            flex.grow,
             {
               paddingTop: Math.max(safeAreaInsets.top, size.default),
             },
@@ -66,56 +124,104 @@ const DashboardScreen = () => {
             gap.default,
           ]}>
           <DashboardPanel />
-          <ScrollView
-            horizontal
-            contentContainerStyle={[
-              flex.flexRow,
-              gap.default,
-              padding.horizontal.default,
-              padding.vertical.small,
-            ]}>
+          <View style={[flex.flexRow, items.start]}>
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={[
+                flex.flexRow,
+                gap.default,
+                padding.horizontal.default,
+                padding.vertical.small,
+              ]}>
+              <SkeletonPlaceholder
+                isLoading={actionNeededTransactions?.length === undefined}>
+                <FilterCard
+                  isActive={activeFilterType === FilterCardType.ActionNeeded}
+                  label={FilterCardType.ActionNeeded}
+                  count={actionNeededTransactions?.length || 0}
+                  onPress={() =>
+                    setActiveFilterType(FilterCardType.ActionNeeded)
+                  }
+                />
+              </SkeletonPlaceholder>
+              <SkeletonPlaceholder
+                isLoading={terminatedTransactions?.length === undefined}>
+                <FilterCard
+                  isActive={activeFilterType === FilterCardType.Terminated}
+                  label="Terminated"
+                  count={terminatedTransactions.length}
+                  onPress={() => setActiveFilterType(FilterCardType.Terminated)}
+                />
+              </SkeletonPlaceholder>
+            </ScrollView>
+          </View>
+          {filteredTransactions && filteredTransactions.length > 0 ? (
             <View
               style={[
                 flex.flex1,
-                rounded.default,
-                {
-                  maxHeight: size.xlarge6,
-                },
-                dimension.width.xlarge8,
-                shadow.small,
-                padding.default,
                 flex.flexCol,
                 gap.default,
+                padding.horizontal.default,
               ]}>
-              <View
-                style={[flex.flex1, flex.flexCol, justify.end, gap.xsmall2]}>
-                <Text className="font-medium" style={[font.size[20]]}>
-                  1 registration
-                </Text>
-              </View>
-              <View>
-                <Text
-                  className="font-bold"
-                  style={[font.size[30], textColor(COLOR.text.neutral.med)]}>
-                  Ongoing Transactions
-                </Text>
-              </View>
+              {filteredTransactions?.map(transaction => (
+                <RegisteredUserListCard
+                  key={transaction.id}
+                  transaction={transaction}
+                  role={UserRole.ContentCreator} //dashboard is focusing on cc side
+                />
+              ))}
             </View>
-          </ScrollView>
-          <View
-            style={[
-              flex.flex1,
-              flex.flexCol,
-              gap.default,
-              padding.horizontal.default,
-            ]}>
-            {transactions.map(transaction => (
-              <CampaignCard key={transaction.id} transaction={transaction} />
-            ))}
-          </View>
+          ) : (
+            <EmptyPlaceholder />
+          )}
         </ScrollView>
       </View>
     </>
+  );
+};
+
+interface FilterCardProps extends PressableProps {
+  isActive: boolean;
+  label: string;
+  count: number;
+}
+
+const FilterCard = ({isActive, label, count, ...props}: FilterCardProps) => {
+  return (
+    <Pressable
+      style={[
+        rounded.default,
+        dimension.width.xlarge8,
+        // shadow.small,
+        padding.default,
+        flex.flexCol,
+        gap.default,
+        background(COLOR.black[1]),
+        border({
+          borderWidth: 1,
+          color: COLOR.black[20],
+        }),
+
+        isActive && [
+          background(COLOR.background.neutral.high),
+          border({
+            borderWidth: 1,
+            color: COLOR.green[50],
+          }),
+          shadow.default,
+        ],
+      ]}
+      {...props}>
+      <Text style={[font.size[20], textColor(COLOR.text.neutral.med)]}>
+        {label}
+      </Text>
+      <Text
+        className="font-bold"
+        style={[font.size[40], textColor(COLOR.text.neutral.high)]}>
+        {count}
+      </Text>
+    </Pressable>
   );
 };
 
@@ -213,7 +319,12 @@ const DashboardPanel = () => {
           )}
         </DashboardPanelItem>
 
-        <DashboardPanelItem label="Rating">
+        <DashboardPanelItem
+          label={
+            user?.contentCreator?.ratedCount
+              ? `${user?.contentCreator?.ratedCount} Rating`
+              : 'Rating'
+          }>
           <View style={[flex.flexRow, items.end, gap.xsmall2]}>
             <RatingStarIcon size="medium" />
             <Text
