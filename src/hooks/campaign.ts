5140300e50b6e7f4f591760461725c99619964f3
winstonkhoe <winstonkhoe@yahoo.com>
2024-01-23 20:24:38 +0700

    fix(campaign-hook): improve ordering of campaigns and adjust campaign hook for admin

--- src/hooks/campaign.ts
+++ src/hooks/campaign.ts
@@ -7,7 +7,7 @@ import {
 } from '../redux/slices/campaignSlice';
 
 export const useOngoingCampaign = () => {
-  const {uid} = useAppSelector(state => state.user);
+  const {uid, isAdmin} = useAppSelector(state => state.user);
   const now = useMemo(() => new Date(), []);
   const {userCampaigns, nonUserCampaigns} = useAppSelector(
     state => state.campaign,
@@ -19,17 +19,27 @@ export const useOngoingCampaign = () => {
       return Campaign.getAll(campaigns => {
         Promise.all(
           campaigns.map(async campaign =>
-            (await campaign.isRegisterable()) ? campaign : null,
+            (await campaign.isRegisterable())
+              ? campaign
+              : isAdmin
+              ? campaign
+              : null,
           ),
         )
           .then(registerableCampaigns => {
+            const filteredCampaigns = registerableCampaigns
+              .filter((campaign): campaign is Campaign => campaign !== null)
+              .filter(c => c.userId !== uid);
+            const upcomingCampaigns = filteredCampaigns
+              .filter(c => c.isUpcomingOrRegistration())
+              .sort(Campaign.sortByTimelineStart);
+            const pastCampaigns = filteredCampaigns
+              .filter(c => !c.isUpcomingOrRegistration())
+              .sort(Campaign.sortByTimelineStart)
+              .reverse();
             dispatch(
               setNonUserCampaigns(
-                registerableCampaigns
-                  .filter((campaign): campaign is Campaign => campaign !== null)
-                  .filter(c => c.userId !== uid)
-                  .sort(Campaign.sortByTimelineStart)
-                  .map(c => c.toJSON()),
+                [...upcomingCampaigns, ...pastCampaigns].map(c => c.toJSON()),
               ),
             );
           })
@@ -38,14 +48,25 @@ export const useOngoingCampaign = () => {
           });
       }, CampaignType.Public);
     }
-  }, [now, uid, dispatch]);
+  }, [now, uid, isAdmin, dispatch]);
 
   useEffect(() => {
     if (uid) {
       console.log('hook:useOngoingCampaign');
       return Campaign.getUserCampaignsReactive(uid, (campaigns: Campaign[]) => {
+        const upcomingCampaigns = campaigns
+          .filter(c => c.isUpcomingOrRegistration())
+          .sort(Campaign.sortByTimelineStart);
+        const pastCampaigns = campaigns
+          .filter(c => !c.isUpcomingOrRegistration())
+          .sort(Campaign.sortByTimelineStart)
+          .reverse();
         dispatch(
-          setUserCampaigns(campaigns.map(campaign => campaign.toJSON())),
+          setUserCampaigns(
+            [...upcomingCampaigns, ...pastCampaigns].map(campaign =>
+              campaign.toJSON(),
+            ),
+          ),
         );
       });
     }
