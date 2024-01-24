35058feeda52a39860bfef516aeea4326a67fbe3
winstonkhoe <winstonkhoe@yahoo.com>
2024-01-24 17:21:12 +0700

    fix(my-campaigns, admin): remove usage to hook, to cover condition for admin

--- src/screens/profile/MyCampaignsScreen.tsx
+++ src/screens/profile/MyCampaignsScreen.tsx
@@ -1,5 +1,4 @@
-import {ScrollView, Text, View} from 'react-native';
-import {CloseModal} from '../../components/atoms/Close';
+import {ScrollView, View} from 'react-native';
 import SafeAreaContainer from '../../containers/SafeAreaContainer';
 import {flex} from '../../styles/Flex';
 import {gap} from '../../styles/Gap';
@@ -12,18 +11,38 @@ import {
   AuthenticatedStack,
 } from '../../navigation/StackNavigation';
 import {EmptyPlaceholder} from '../../components/templates/EmptyPlaceholder';
-import {useOngoingCampaign} from '../../hooks/campaign';
 import {PageWithBackButton} from '../../components/templates/PageWithBackButton';
 import {BackButtonLabel} from '../../components/atoms/Header';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {size} from '../../styles/Size';
+import {useEffect, useState} from 'react';
+import {LoadingScreen} from '../LoadingScreen';
 type Props = NativeStackScreenProps<
   AuthenticatedStack,
   AuthenticatedNavigation.MyCampaigns
 >;
 const MyCampaignsScreen = ({route}: Props) => {
-  const {userCampaigns} = useOngoingCampaign();
-  const safeAreaInsets = useSafeAreaInsets();
+  const {userId} = route.params;
+  const [campaigns, setCampaigns] = useState<Campaign[]>();
+  const isEmptyCampaign = campaigns?.length === 0;
+
+  useEffect(() => {
+    if (userId) {
+      return Campaign.getUserCampaignsReactive(userId, (cs: Campaign[]) => {
+        const upcomingCampaigns = cs
+          .filter(c => c.isUpcomingOrRegistration())
+          .sort(Campaign.sortByTimelineStart);
+        const pastCampaigns = cs
+          .filter(c => !c.isUpcomingOrRegistration())
+          .sort(Campaign.sortByTimelineStart)
+          .reverse();
+        setCampaigns([...upcomingCampaigns, ...pastCampaigns]);
+      });
+    }
+  }, [userId]);
+
+  if (campaigns === undefined) {
+    return <LoadingScreen />;
+  }
 
   return (
     <PageWithBackButton
@@ -31,12 +50,15 @@ const MyCampaignsScreen = ({route}: Props) => {
       backButtonPlaceholder={<BackButtonLabel text="My Campaigns" />}
       threshold={0}>
       <ScrollView
-        style={[
-          {
-            paddingTop: Math.max(safeAreaInsets.top, size.default),
-          },
-        ]}
-        contentContainerStyle={[flex.flex1]}>
+        style={[]}
+        contentContainerStyle={[
+          !isEmptyCampaign && [
+            {
+              paddingTop: size.xlarge3,
+            },
+          ],
+          flex.flex1,
+        ]}>
         <SafeAreaContainer enable>
           <View
             style={[
@@ -45,10 +67,10 @@ const MyCampaignsScreen = ({route}: Props) => {
               gap.medium,
               padding.horizontal.default,
             ]}>
-            {userCampaigns.length <= 0 ? (
+            {isEmptyCampaign ? (
               <EmptyPlaceholder />
             ) : (
-              userCampaigns.map((c: Campaign, index: number) => (
+              campaigns.map((c: Campaign, index: number) => (
                 <CampaignCard campaign={c} key={index} />
               ))
             )}
